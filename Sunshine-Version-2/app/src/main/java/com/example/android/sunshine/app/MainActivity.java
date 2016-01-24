433c18a7f2ed458f54e0c03f090eec628e22ba3e
felinus <mong87suk@google.com>
2016-01-24 10:09:31 +0900

    Test UI test on other thread which is not main thread
    Signed-off-by: felinus <mong87suk@google.com>

--- Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.sunshine.app;
 
+import android.os.Looper;
+import android.text.format.Time;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.v7.app.ActionBarActivity;
@@ -13,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -28,20 +31,68 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import android.os.Process;
 
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = "Main";
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
+
+        HandlerThread handlerThread = new HandlerThread("network_connection_test");
+        handlerThread.start();
+        //Log.v(TAG, "thread id:" + handlerThread.getThreadId());
+
+        Log.v(TAG, "handlerThread.getLooper()" + handlerThread.getLooper());
+        Log.v(TAG, "main thread tid" + Looper.getMainLooper());
+        Log.v(TAG, "current thread:" + handlerThread.getLooper().isCurrentThread());
+        Log.v(TAG, "main priority:" + Process.getThreadPriority(Process.myTid()));
+        handler = new Handler(handlerThread.getLooper());
+        Log.v(TAG, "test");
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                TextView test = (TextView) findViewById(R.id.test);
+                test.setText("test");
+                Log.v(TAG, "other priority:" + Process.getThreadPriority(Process.myTid()));
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+        HandlerThread handlerThread = new HandlerThread("network_connection_test");
+        handlerThread.start();
+
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                TextView test = (TextView) findViewById(R.id.test);
+                test.setText("test");
+            }
+        });*/
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.v(TAG, "other priority:" + Process.getThreadPriority(Process.myTid()));
+                TextView test = (TextView) findViewById(R.id.test);
+                test.setText("test");
+            }
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
